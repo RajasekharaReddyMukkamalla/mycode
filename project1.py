f72857d6582161811299eef16daf60e2f1533162
RajasekharaReddyMukkamalla <37010280+RajasekharaReddyMukkamalla@users.noreply.github.com>
2019-12-30 20:42:10 +0530

    Add files via upload
    modified new file

--- project1.py
+++ project1.py
@@ -2,20 +2,31 @@
 import sys
 import mysql.connector
 mydb = mysql.connector.connect(
-@ -10,7 +10,7 @@ mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="Raja@1234",
+  database="mydatabase"
+)
 mycursor = mydb.cursor()
 
 mycursor.execute("TRUNCATE TABLE BaseTable2")
-mycursor.execute("TRUNCATE TABLE Sample")
 mycursor.execute("TRUNCATE TABLE STG_Table")
 
 f = open(sys.argv[1], "r")
 l1=[6,6,20,5,7,30,6,4,12,20,11,11,11]
-@ -27,12 +27,16 @@ f.close()
+for x in f:
+  l2=[]	
+  for i in range(len(l1)):
+    l2.append(f.read(l1[i]))
+  mycursor = mydb.cursor()
+  if(x):
+    sql = "INSERT INTO BaseTable2 (TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = (l2[0],l2[1],l2[2],l2[3],l2[4],l2[5],l2[6],l2[7],l2[8],l2[9],l2[10],l2[11],l2[12])
+  mycursor.execute(sql, val)
+f.close()
 
 mycursor.execute("DELETE FROM BaseTable2 WHERE TransactionId LIKE ProductQuantity")
 
-sql = "INSERT INTO BaseTable3 (TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate) SELECT TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, COALESCE(STR_TO_DATE(TransactionTimeStamp, '%d-%b-%Y %H:%i:%S'),STR_TO_DATE(TransactionTimeStamp, '%d/%b/%Y %H:%i:%S'),STR_TO_DATE(TransactionTimeStamp, '%d %b %Y %H:%i:%S')),OrderedDate, ShipmentDate, DeliveredDate FROM BaseTable2"
 sql = "INSERT INTO STG_Table(TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate) SELECT TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, COALESCE(STR_TO_DATE(TransactionTimeStamp, '%d-%b-%Y %H:%i:%S'),STR_TO_DATE(TransactionTimeStamp, '%d/%b/%Y %H:%i:%S'),STR_TO_DATE(TransactionTimeStamp, '%d %b %Y %H:%i:%S')),OrderedDate, ShipmentDate, DeliveredDate FROM BaseTable2"
 mycursor.execute(sql)
 sql="UPDATE STG_Table S set S.Action_Index = 'U' where S.TransactionId in (SELECT b.TransactionId from BaseTable3 b where b.Active_Index = 'Y') "
@@ -23,10 +34,6 @@ mycursor.execute(sql)
 sql="UPDATE BaseTable3 b set b.Active_Index = 'N' where b.TransactionId in (SELECT S.TransactionId from STG_Table S where S.Action_Index = 'U') "
 mycursor.execute(sql)
 
-mycursor.execute("INSERT INTO Sample select * from ( select * , ROW_NUMBER () over (partition by TransactionId order by TransactionTimeStamp desc,FIELD(Status,'Delivered   ','Shipped     ','Ordered     ')) as rownum  from BaseTable3 ) b1")
-mycursor.execute("UPDATE Sample SET Active_Index='N' where row_num > 1")
-mycursor.execute("TRUNCATE TABLE BaseTable3")
-mycursor.execute("INSERT INTO BaseTable3(TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate, Active_Index) select TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate, Active_Index from Sample")
 sql = "INSERT INTO BaseTable3 (TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate,Active_Index) SELECT TransactionId, CustomerId, CustomerName, CustAddId, ProductId, ProductName, ProductPrice, ProductQuantity, Status, TransactionTimeStamp, OrderedDate, ShipmentDate, DeliveredDate, Action_Index FROM STG_Table"
 mycursor.execute(sql)
 sql = "UPDATE BaseTable3 set Active_Index = 'Y' where Active_Index = 'I' or Active_Index = 'U'"
